4ec0c3f6f361c9267f97a27ec5f86cba504d2408
ChenZakaim <chenzakaim@hilma.tech>
2023-10-15 17:13:09 +0300

    added registration

---
+++ javascript/registration.js
@@ -0,0 +1,13 @@
+
+
+
+addFormInputs("log in");
+const formBox = document.getElementById("formBox");
+const form = document.getElementsByTagName("form")[0];
+const title = document.getElementById("title");
+const submit = document.getElementById("submit");
+
+
+
+    
+
