037b9afdc326977748a2a879dc630cfdb85e441b
Shirshlomo18 <Shirshlomo@hilma.tech>
2023-10-15 21:01:46 +0300

    try

---
+++ javascript/2048.js
@@ -0,0 +1,33 @@
+// let board;
+// let rows=4;
+// let columns=4;
+// let score=0;
+// window.onload()=function(){
+//   game();
+// }
+// function game(){
+//     board=[[0, 0, 0, 0]
+//     [0, 0, 0, 0]
+//     [0, 0, 0, 0]
+//     [0, 0, 0, 0]]
+// }
+//     for(let r=0;r<rows;r++){
+//         for(let c=0;  c<columns;c++){
+//            let title = document.createElement("div");
+//            title.id=board[r].toString()+board[c].toString();
+//            let num =board[r]+board[c];
+//            updateBoard(title, num);
+//            document.getElementById("board").append(title);
+//         }
+//         }
+// function updateBoard(title,num){
+//     title.innerText="";
+//     title.classList.value="";
+//     title.classList.add("title")
+//          if(num<=4096){
+//             title.classList.add("x"+num.toString())
+//          }else {
+//             title.classList.add("x8192")
+//          }
+//         }
+
