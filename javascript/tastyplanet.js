c4184987ca9ef2c09578b7280cc63ded2ca53892
Shirshlomo18 <Shirshlomo@hilma.tech>
2023-10-16 14:29:07 +0300

    tryagain

--- javascript/tastyplanet.js
+++ javascript/tastyplanet.js
@@ -1,15 +1,23 @@
 
+let grid= document.getElementById("grid-container").addEventListener("click",go());
 let dotone= document.getElementById("dotone").addEventListener("click",go());
-let x =Math.floor(math.random()*19+1);
+let x=Math.floor(math.random()*19+1);
 let y= Math.floor(math.random()*19+1);
-window.onload=function(){
+window.onload=function(x,y){
     dotone.style.setProperty("grid-column-start", ""+x);
     dotone.style.setProperty("grid-row-start",""+ y);
 }
+
 function go(){
-    shark=dotone[x,y];
-    shark.style.height+=5;
-    shark.style.width+=5;
+    let i =document.getElementById("shark");
+    i.style.height+=5px;
+    i.style.width+=5px;
      x= Math.floor(math.random()*19+1);
      y= Math.floor(math.random()*19+1);
+     set(x,y);
 }  
+
+function set (x, y){
+    dotone.style.setProperty("grid-column-start", ""+x);
+    dotone.style.setProperty("grid-row-start",""+ y);
+}
