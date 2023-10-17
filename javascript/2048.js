843dc38a4484209d49ce038b6b8826e5e265660d
ChenZakaim <chenzakaim@hilma.tech>
2023-10-17 12:27:41 +0300

    started working on the 2048 game

--- javascript/2048.js
+++ javascript/2048.js
@@ -1,79 +1,93 @@
-// const board = 
-// [[0, 0, 0, 0]
-//  [0, 0, 0, 0]
-//  [0, 0, 0, 0]
-//  [0, 0, 0, 0]];
-// const rows = 4;
-// const columns = 4;
-// let score = 0;
-// for (let t =0; t<=1; t++) {
-//     let r = Math.floor(Math.random() * 4);
-//     let c = Math.floor(Math.random() * 4);
-//     board[r][c] = 2;
-// }
+
+let board = createBoard();
+updateHtmlBoard(board);
+
+document.getElementById("board").addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowUp":
+            //arrow up: scan from top to buttom, left to roght
+            for (let row = 0; row < arrBoard.length; row++) {
+                for (let col = 0; col < arrBoard[row].length; col++) {
+                    if (board[row][col] === 0) {
+                        for (let i = row + 1; i < board.length; i++) {
+                            board[i-1][col] = board[i][col];
+                        }
+                    }
+                    else if (board[row][col] === board[row + 1][col]){
+                        board[row][col] += board[row + 1][col];
+                        board[row + 1][col] = 0;
+                        addToScore(board[row][col]);
+                    }                 
+                }
+            }
+updateHtmlBoard(board);
 
 
 
-// document.getElementById()
 
 
 
-// for (let r = 0; r < rows; r++) {
-//     for (let c = 0; c < columns; c++) {
-//         let tile = document.createElement("div");
-//         title.id =""+r+c;
-//         let num = board[r] + board[c];
-//         updateBoard(title, num);
-//         document.getElementById("board").append(title);
-//     }
-// }
-// function updateBoard(tile, num) {
-//     // title.innerText = title.id.
-//     tile.classList.value = "";
-//     tile.classList.add("tile")
-//     if (num <= 4096) {
-//         tile.classList.add("x" + num.toString())
-//     } else {
-//         tile.classList.add("x8192")
-//     }
-// }
-// let i = document.getElementsByClassName("tile");
 
-// function filterZero(row) {
-//     return row.filter(num => num != 0);
-// }
-// function slide() {
 
-//     for (let i = 0; i < row.length; i++) {
-//         [
-//         if (row[i] == row[I++]) {
-//             row[i] *= 2;
-//             row[i + 1] = 0;
-//             score += row[i]
-//         }
-//     }
-//     row = filterZero(row);
-//     while (row.length < columns) { row.push(0) }
-// }
-// i.addEventListener("keyup", function (event) {
-//     switch (event.keyCode) {
-//         case 37:
-//             for (let r0 = 0; r < rows; r++) {
-//                 let row = board[r];
-//                 row = slide(row)
-//                 board[r] = row;
+            break;
+        case "ArrowDown":
+            break;
+        case "ArrowLeft":
+            break;
+        case "ArrowRight":
+            break;
+    }
+    updateHtmlBoard(arrBoard);
+});
 
-//             }
-//     }
-// }
+function createBoard() {
+    const board =
+        [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]];
+    for (let t = 0; t <= 1; t++) {
+        let r = Math.floor(Math.random() * 4);
+        let c = Math.floor(Math.random() * 4);
+        board[r][c] = 2;
+    }
+    console.log('board: ', board);
+    return board;
+}
 
-//         break;
-//     }
-//     case 38:
-// break;
-//     case 39:
-// break;
-//     case 40:
-// break;
-// })
+
+function updateHtmlBoard(arrBoard) {
+    let htmlBoard = document.getElementById("board");
+
+    let isEmpty = !htmlBoard.hasChildNodes()
+    for (let row = 0; row < arrBoard.length; row++) {
+        for (let col = 0; col < arrBoard[row].length; col++) {
+            console.log('isEmpty: ', isEmpty);
+            if (isEmpty) {
+                let tile = document.createElement("div");
+                tile.id = "" + row + col;
+                htmlBoard.append(tile);
+            }
+            else {
+                let tile = document.getElementById(""+row+col);    
+            }
+            tile.className = "tile x" + arrBoard[row][col];
+            tile.textContent = arrBoard[row][col];
+        }
+    }
+}
+
+function addToScore(num) {
+    const htmlScore = getElementById("score");
+    let score = parseInt(JSON.parse(localStorage.getItem("scoreLastGame2048"))) || 0;
+    let bestScore = parseInt(JSON.parse(localStorage.getItem("bestScore2048"))) || 0;
+
+    score += num;
+    htmlScore.textContent = score;
+
+    if (score > bestScore) {
+        localStorage.setItem("bestScoreGame2048", score);
+    }
+    localStorage.setItem("scoreLastGame2048", score);
+}
 
